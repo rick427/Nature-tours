c9344f2fd6eeff8affcfd718f7da16c7c85b4269
rick427 <rchardnjoku@yahoo.com>
2019-08-17 11:43:03 +0100

    find tour by id

--- app.js
+++ app.js
@@ -18,6 +18,27 @@ app.get('/api/v1/tours', (req, res) => {
     })
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    //console.log(req.params.id);
+    //const id = req.params.id * 1;
+    /*This is because the value of :id in params is a string*/
+    const id = JSON.parse(req.params.id);
+    const tour = tours.find(tour => tour.id === id);
+
+    //if (id > tours.length) {
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            Message: 'Not Found..'
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: tour
+    })
+});
+
 app.post('/api/v1/tours', (req, res) => {
     //console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
