99aa8966efe726e1093d56ca17f717202ee608f8
rick427 <rchardnjoku@yahoo.com>
2019-08-17 18:04:51 +0100

    env variables

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const morgan = require('morgan');
 const app = express();
 
 //:MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './config.env'
+});
 const app = require('./app');
 
+
 //: SERVER
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
