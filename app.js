7ac9b22d9e5c70728a41afc31f2a39a1a2831737
rick427 <rchardnjoku@yahoo.com>
2019-08-17 11:23:00 +0100

    creating a new tour

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
+//middlewares
+app.use(express.json());
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
@@ -13,7 +16,25 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     })
-})
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    //console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({
+        id: newId
+    }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        if (err) return console.log(err);
+        res.status(201).json({
+            status: 'success',
+            data: newTour
+        })
+    });
+});
 
 const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
