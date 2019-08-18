3bb03fd018b2716800f084f31e1f75217d2105ac
rick427 <rchardnjoku@yahoo.com>
2019-08-18 18:06:05 +0100

    finishing up tour controller refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -56,20 +56,38 @@ exports.createTour = async (req, res) => {
     }
 }
 
-exports.updateTour = (req, res) => {
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tours.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Updated Tour'
-        }
-    })
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 }
 
-exports.deleteTour = (req, res) => {
-
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
+exports.deleteTour = async (req, res) => {
+    try {
+        await Tours.findByIdAndRemove(req.params.id)
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 }
