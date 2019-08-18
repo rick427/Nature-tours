2bbe1b9f83e3786a9c09ce27849f6ea16c5b155e
rick427 <rchardnjoku@yahoo.com>
2019-08-18 19:17:28 +0100

    script to delete and import json data to database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+require('dotenv').config({
+    path: './config.env'
+});
+const fs = require('fs');
+const Tours = require('../../models/Tour');
+
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+(async () => {
+    try {
+        await mongoose.connect(db, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        console.log('Database Connection Secured..');
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+//READ FILE
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+//IMPORT DATA INTO DB
+const importData = async () => {
+    try {
+        await Tours.create(tours);
+        console.log('Data successfully loaded!..');
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//DELETE ALL DATA FROM DB
+const deleteData = async () => {
+    try {
+        await Tours.deleteMany();
+        console.log('Data successfully deleted!..');
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+if (process.argv[2] === '--import') {
+    importData()
+} else if (process.argv[2] === '--delete') {
+    deleteData()
+}
+//console.log(process.argv);
