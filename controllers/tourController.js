1229422f19f43d9d6f9f163536860506013546e8
rick427 <rchardnjoku@yahoo.com>
2019-08-18 20:52:18 +0100

    added limiting and disabling createAt from res

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,10 +21,17 @@ exports.getAllTours = async (req, res) => {
         // 2) SORTING
         if (req.query.sort) {
             const sortBy = req.query.sort.split(',').join(' ');
-            console.log(sortBy)
             query = query.sort(sortBy);
         } else {
             query = query.sort('-createdAt');
+        }
+
+        //3) LIMITING
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields)
+        } else {
+            query = query.select('-__v')
         }
 
         //: EXECUTE QUERY
