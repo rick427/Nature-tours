fd35d6220a61ad3155b807c851b5043ad1a97dbd
rick427 <rchardnjoku@yahoo.com>
2019-08-23 18:06:39 +0100

    error handling middleware

--- app.js
+++ app.js
@@ -17,4 +17,27 @@ const userRouter = require('./routes/userRoutes');
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Cannot find ${req.originalUrl} on this server`
+    // })
+    const err = new Error(`Cannot find ${req.originalUrl} on this server`);
+    err.status = 'fail',
+    err.statusCode = 404
+
+    next(err);
+});
+
+//:ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error'
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
+
 module.exports = app;
