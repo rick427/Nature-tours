3c37d32b34ccd3feb008c432d8f969525320736a
rick427 <rchardnjoku@yahoo.com>
2019-08-24 09:53:45 +0100

    uncaught rejection error catching

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ require('dotenv').config({
   path: './config.env'
 });
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 //: DATABASE CONNECTION
@@ -24,8 +30,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
