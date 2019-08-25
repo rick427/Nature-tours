0ee2afbfd00f22756fa5881029fb984aac712377
rick427 <rchardnjoku@yahoo.com>
2019-08-25 16:58:05 +0100

    send jwt via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,10 +14,18 @@ const signedToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signedToken(user._id);
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+    
+    if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
-    // res.cookie('jwt', token, {
-    //     expires: 
-    // })
+    res.cookie('jwt', token, cookieOptions);
+
+    user.password = undefined;
 
     res.status(statusCode).json({
         status: 'success',
