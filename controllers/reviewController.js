4344f64315dced7beb2b0b75ccadad6a0ddadb23
rick427 <rchardnjoku@yahoo.com>
2019-08-31 19:38:17 +0100

    get all reviews on tour and by id

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,10 @@ const Review = require('../models/Review');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if(req.params.tourId) filter = {tour: req.params.tourId}
+  
+  const reviews = await Review.find(filter);
   res.status(200).json({
       status: 'success',
       result: reviews.length,
@@ -16,7 +19,7 @@ exports.createReview = catchAsync(async (req,res,next) => {
     // Allow nested routes
     if(!req.body.tour) req.body.tour = req.params.tourId
     if(!req.body.user) req.body.user = req.user.id
-    
+
     const newReview = await Review.create(req.body);
     res.status(201).json({
         status:' success',
