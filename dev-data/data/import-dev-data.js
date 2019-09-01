606f78d87f495088958b979ff04811adde336b52
rick427 <rchardnjoku@yahoo.com>
2019-09-01 17:13:54 +0100

    re-importing data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,6 +4,8 @@ require('dotenv').config({
 });
 const fs = require('fs');
 const Tours = require('../../models/Tour');
+const User = require('../../models/User');
+const Review = require('../../models/Review');
 
 const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
@@ -22,11 +24,15 @@ const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
 //READ FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 //IMPORT DATA INTO DB
 const importData = async () => {
     try {
         await Tours.create(tours);
+        await User.create(users, {validateBeforeSave: false});
+        await Review.create(reviews);
         console.log('Data successfully loaded!..');
         process.exit();
     } catch (error) {
@@ -38,6 +44,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tours.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('Data successfully deleted!..');
         process.exit();
     } catch (error) {
