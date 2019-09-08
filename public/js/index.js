c5504e764d39e09e87a708c47559388e4093cba0
rick427 <rchardnjoku@yahoo.com>
2019-09-08 13:52:12 +0100

    logging out users

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 import '@babel/polyfill';
 import {displayMap} from './mapbox';
-import {login} from './login';
+import {login, logout} from './login';
 
 //DOM Elements
 const mapBox = document.getElementById('map')
 const loginForm = document.querySelector('.form');
+const logoutBtn = document.querySelector('.nav__el--logout')
 
 // DELEGATION
 if(mapBox){
@@ -19,4 +20,8 @@ if(loginForm){
         const password = document.getElementById('password').value;
         login(email, password);
     })
+}
+
+if(logoutBtn){
+    logoutBtn.addEventListener('click', logout)
 }
