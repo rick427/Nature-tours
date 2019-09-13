fa83ba41ac1b1b12476a6a95848ed1372e5457b7
rick427 <rchardnjoku@yahoo.com>
2019-09-13 08:15:54 +0100

    resizing images with the "sharp" library

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,7 +16,7 @@ router.use(authController.protected);
 
 router.patch('/updatePassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser)
-router.patch('/updateMe', userController.uploadUserPhoto, userController.updateMe);
+router.patch('/updateMe', userController.uploadUserPhoto, userController.resizeUserPhoto, userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 
