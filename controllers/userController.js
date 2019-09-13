f997d398cccabfc248318cd6c1a549a6d559a51c
rick427 <rchardnjoku@yahoo.com>
2019-09-13 07:27:35 +0100

    updating user image

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,8 +47,6 @@ exports.getMe = (req, res, next) => {
 //:ROUTE HANDLERS
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-    console.log(req.file);
-    console.log(req.body);
 
    // 1) create error if user tries to update password
    if(req.body.password || req.body.passwordConfirm) {
@@ -57,6 +55,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
    // 2) filter fields names that shouldnt be updated
    const filteredBody = filterObj(req.body, 'name', 'email');
+   if(req.file) filteredBody.photo = req.file.filename;
 
    //3) update user document
    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
